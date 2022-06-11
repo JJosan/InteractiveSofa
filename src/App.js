ee569955a3506b8e5d728758dd4471ea9ecff60e
JJosan <nosajn123@gmail.com>
2022-06-11 01:16:48 -0700

    cool it works

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Suspense } from 'react';
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import { Canvas, useLoader, useFrame } from "@react-three/fiber";
+import { PresentationControls} from '@react-three/drei'
+
+const Scene = () => {
+  const sofa = useLoader(FBXLoader, "sofa.fbx");
+  useFrame(({clock}) => {
+    const t = clock.getElapsedTime();
+    sofa.rotation.y = t
+  })
+  return <primitive object={sofa} scale={0.02} />;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Canvas>
+        <ambientLight intensity={.2} color={"lightgreen"}/>
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} shadow-mapSize={[512, 512]} castShadow />
+        <Suspense fallback={null}>
+          <PresentationControls
+            global
+            config={{ mass: 2, tension: 500 }}
+            snap={{ mass: 4, tension: 1500 }}
+            rotation={[.5, 0, 0]}
+            polar={[-Math.PI / 3, Math.PI / 3]}
+            azimuth={[-Math.PI / 1.4, Math.PI / 2]}>
+            <Scene />
+          </PresentationControls>
+        </Suspense>
+      </Canvas>
     </div>
+    
   );
 }
 
